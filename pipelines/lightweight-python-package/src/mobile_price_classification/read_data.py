70c81710570e340e7493001151360325300c67cd
Timo Brockmann <timo.brockmann@prokube.ai>
2026-03-12 11:37:58 +0100

    Reduce the complexity of the lightweight-components and lightweight-python-package example

--- pipelines/lightweight-python-package/src/mobile_price_classification/read_data.py
+++ pipelines/lightweight-python-package/src/mobile_price_classification/read_data.py
@@ -1,4 +1,3 @@
-import os
 import pandas as pd
 
 
@@ -8,15 +7,8 @@ def read_data(
     train_output_path: str,
     test_output_path: str,
 ):
-    """Reads training and test data from MinIO and writes to parquet files."""
-    storage_options = {
-        "key": os.environ.get("AWS_ACCESS_KEY_ID"),
-        "secret": os.environ.get("AWS_SECRET_ACCESS_KEY"),
-        "client_kwargs": {"endpoint_url": "http://minio.minio"},
-    }
-
-    df_train = pd.read_csv(minio_train_data_path, storage_options=storage_options)
-    df_test = pd.read_csv(minio_test_data_path, storage_options=storage_options)
+    df_train = pd.read_csv(minio_train_data_path)
+    df_test = pd.read_csv(minio_test_data_path)
 
     df_train.to_parquet(train_output_path)
     df_test.to_parquet(test_output_path)
